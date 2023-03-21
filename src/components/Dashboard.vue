ae68084ded688fe5f6fdb211561559dec52504d4
lyx <634889443@qq.com>
2023-03-21 21:04:45 +0800

    add tables

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,50 +1,52 @@
 <template>
-    <div style="margin: 20px;">
+    <div>
+        <button @click="QueryCollection"> gets</button>
+    </div>
+    <div style="margin-left: 20px;margin-right: 20px;">
         <el-table :data="dataTAble" style="width: 100%">
-            <el-table-column prop="date" label="Date" width="180" />
             <el-table-column prop="name" label="Name" width="180" />
-            <el-table-column prop="address" label="Address" />
+            <el-table-column prop="type" label="type" width="180" />
+            <el-table-column prop="describe" label="describe" />
+            <el-table-column prop="DateTime" label="DateTime" />
         </el-table>
     </div>
 
-    <Idv>
-        <button @click="getQueryCollection"> gets</button>
-    </Idv>
+
+    <!-- {{ dataTAble }} -->
 </template>
 
 <script lang="ts" setup>
 import { map } from 'lodash';
 import Files from '../libs/axiosLibs/Files';
 import { timelineItemProps } from 'element-plus';
-import{ref} from 'vue'
+import { ref } from 'vue'
 let FileService = new Files()
-
-var dataTAble:any[]
-
+let names = ref<any[]>([])
+var dataTAble = ref<any[]>([])
 let QueryCollection = async () => {
-    let dataTAble:any[]=[]
+
     var data = await FileService.QueryAllColletionInfo()
-    console.log(data)
+
     for (var item in data) {
         for (let key in data[item]) {
-            console.log(key);
             var res: any[] = data[item][key]
-            let temp:Map<string,any>=new Map()
-            temp.set("Name",key)
-            for(let items in res){
-                for (let item in res[items]){
-                    temp.set(item,res[items][item])
+            let temp: Map<string, any> = new Map()
+            temp.set("name", key)
+            for (let items in res) {
+                for (let item in res[items]) {
+                    temp.set(item, res[items][item])
                 }
             }
-            dataTAble.push(temp)
-            console.log(temp)
+            const myobject = [...temp].reduce((obj: any, [key, value]) => {
+                obj[key] = value
+                return obj
+            }, {})
+            dataTAble.value.push(myobject)
+            console.log(myobject)
         }
-    }   
+    }
+    // return dataTAble
     console.log(dataTAble)
-    return dataTAble
-}
-let  getQueryCollection=async()=>{
-    dataTAble=  await QueryCollection()
 }
 
 // const tableData = [
